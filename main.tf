b9331690f480c0354a90a3658558374a07655683
Felicita Dean <Felicita>
2023-11-10 13:34:27 -0600

    module

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
-resource "aws_instance" "webserver" {
-  ami           = "ami-0e8a34246278c21e4"
+resource "aws_instance" "webserver"{
+  ami           = "ami-059cdad1f35de63c0"
   instance_type = "t2.micro"  
 
   tags = {
     Name = "Terraform-server"
   }
   }
+  
  
